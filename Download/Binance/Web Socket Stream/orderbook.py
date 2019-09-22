1f30320919ff8af675c64e8ea8018d50dfb70e2b
Alfred Holmes <holmesey55@gmail.com>
2019-09-22 12:39:02 +0100

    Work on basic markov model approximations

--- Download/Binance/Web Socket Stream/orderbook.py
+++ Download/Binance/Web Socket Stream/orderbook.py
@@ -1,6 +1,7 @@
 #wss://stream.binance.com:9443/ws/btcusdt@depth
-from websocket import create_connection
-import requests, json, os
+import os, time, asyncio
+import websockets, requests
+import json
 
 def get_orderbook(symbol, limit=1000):
     orderbook = requests.get('https://www.binance.com/api/v1/depth?symbol=' + str(symbol) + '&limit=' + str(limit))
@@ -10,13 +11,13 @@ def get_orderbook(symbol, limit=1000):
         response = json.loads(orderbook.text)
         return response['lastUpdateId'], {float(price): float(quantity) for price, quantity in response['bids']}, {float(price): float(quantity) for price, quantity in response['asks']}
 
+async def process(queue):
+    while True:
+        
 
-def main():
-    ws = create_connection('wss://stream.binance.com:9443/ws/btcusdt@depth@100ms')
+async def main():
 
     last_id, bids, asks = get_orderbook('BTCUSDT', 5000)
-
-
 
     with open(str(last_id) + '_starting_orderbook.json', 'w') as jsonfile:
         json.dump({'bids': bids, 'asks': asks}, jsonfile)
@@ -25,39 +26,54 @@ def main():
     i = 1
     cache = []
 
-    while True:
-        process = json.loads(ws.recv()) # TODO: Need to handle this not returning anything for a while
 
-        bid_changes = {}
-        ask_changes = {}
-        for price, quantity in process['b']:
-            if float(price) in bids:
-                bid_changes[float(price)] = float(quantity) - bids[float(price)]
-                if float(quantity) == 0:
-                    bids.pop(float(price)) #to save memory, doesn't need to happen
+    async with websockets.connect('wss://stream.binance.com:9443/ws/btcusdt@depth@100ms') as ws:
+        previous_time = time.time()
+
+        while True:
+             # TODO: Need to handle this not returning anything for a while
+            print(time.time() - previous_time)
+            try:
+                process = await asyncio.wait_for(ws.recv(), 0.2 - (time.time() - previous_time))
+                print('process')
+            except asyncio.TimeoutError:
+                print('timeout')
+                break
+
+            process = json.loads(process)
+
+            bid_changes = {}
+            ask_changes = {}
+            for price, quantity in process['b']:
+                if float(price) in bids:
+                    bid_changes[float(price)] = float(quantity) - bids[float(price)]
+                    if float(quantity) == 0:
+                        bids.pop(float(price)) #to save memory, doesn't need to happen
+                    else:
+                        bids[float(price)] = float(quantity)
                 else:
-                    bids[float(price)] = float(quantity)
-            else:
-                bid_changes[float(price)] = float(quantity)
+                    bid_changes[float(price)] = float(quantity)
 
-        for price, quantity in process['a']:
-            if float(price) in asks:
-                ask_changes[float(price)] = float(quantity) - asks[float(price)]
-                if float(quantity) == 0:
-                    asks.pop(float(price))
+            for price, quantity in process['a']:
+                if float(price) in asks:
+                    ask_changes[float(price)] = float(quantity) - asks[float(price)]
+                    if float(quantity) == 0:
+                        asks.pop(float(price))
+                    else:
+                        asks[float(price)] = float(quantity)
                 else:
-                    asks[float(price)] = float(quantity)
-            else:
-                ask_changes[float(price)] = float(quantity)
+                    ask_changes[float(price)] = float(quantity)
 
-        data = {'start_id': process['U'], 'end_id': process['u'], 'bid_changes': bid_changes, 'ask_changes': ask_changes}
-        cache.append(data)
-        if len(cache) >= 10 * 60: #write to file every minute
-            with open(str(last_id) + '/' + str(i) + '.json', 'w') as jsonfile:
-                json.dump(cache, jsonfile)
-            cache = []
-            i += 1
+            data = {'start_id': process['U'], 'end_id': process['u'], 'bid_changes': bid_changes, 'ask_changes': ask_changes}
+            cache.append(data)
+            if len(cache) >= 10: #write to file every minute
+                with open(str(last_id) + '/' + str(i) + '.json', 'w') as jsonfile:
+                    json.dump(cache, jsonfile)
+                cache = []
+                i += 1
 
 
 if __name__ == '__main__':
-    main()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
